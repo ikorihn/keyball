8fc0cc32129cba841c1e340b5da1d7f568859f0a
ikorihn <16367098+ikorihn@users.noreply.github.com>
2023-10-30 11:59:46 +0900

    update: backspace and shift when held

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , LT(1, KC_F) , KC_G  ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , LT(1, KC_QUOT),
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RBRC  ,
-              KC_LALT, KC_LGUI,  LT(2, KC_SPC), KC_SPC, LT(1, KC_BSLS),                             KC_BSPC, LT(2, KC_ENT), _______,     _______  , LT(3, KC_LBRC)
+              KC_LALT, KC_LGUI,  LT(2, KC_SPC), KC_SPC, LT(1, KC_BSLS),                             RSFT_T(KC_BSPC), LT(2, KC_ENT), _______,     _______  , LT(3, KC_LBRC)
   ),
 
   [1] = LAYOUT_universal(
